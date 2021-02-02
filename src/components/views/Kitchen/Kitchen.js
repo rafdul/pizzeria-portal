e4f75e5a85aae90d2a2d5fc84f0cedc5e0b565df
rafdul <rafalduluk@gmail.com>
2021-02-02 14:53:45 +0100

    Add datepicker in Tables component

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+// import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import { DataGrid } from '@material-ui/data-grid';
+
 import styles from './Kitchen.module.scss';
+
+
+const columnsOrders = [
+  {field: 'id', headerName: 'ID', width: 80},
+  {field: 'table', headerName: 'Table / Take-a-way', width: 160},
+  {field: 'products', headerName: 'Products', width: 320},
+];
+
+const rowsOrders = [
+  {id: 1, table: 'table2', products: 'pizza, coffee, cake'},
+  {id: 2, table: 'table1', products: '2 x pizza, 2 x coffee, water'},
+  {id: 3, table: 'table1', products: 'pizza, cola'},
+  {id: 4, table: 'table3', products: 'pizza, water, cake'},
+  {id: 5, table: 'table1', products: '3 x pizza, 3 x tee, 2 x ice-cream'},
+  {id: 6, table: 'table2', products: 'coffee, cake'},
+  {id: 7, table: 'table3', products: 'pizza, coffee, ice-cream'},
+  {id: 8, table: 'take-a-way', products: 'pizza, coffee, cake'},
+  {id: 9, table: 'take-a-way', products: 'pizza, coffee, cake'},
+  {id: 10, table: 'table2', products: 'coffee, cake'},
+  {id: 11, table: 'take-a-way', products: 'pizza, coffee, cake'},
+];
 
 const Kitchen = () => (
 
-  <div className={styles.component}>
-    <h2>Kitchen view</h2>
-  </div>
+
+  <Grid container component='div' className={styles.component}>
+    <CssBaseline />
+    <Grid item xs={12}>
+      <Paper className={styles.paper} align='center'>
+        <p className={styles.title}>Orders</p>
+        <div className={styles.orders}>
+          <DataGrid rows={rowsOrders} columns={columnsOrders} pageSize={10} checkboxSelection disableColumnMenu/>
+        </div>
+      </Paper>
+    </Grid>
+  </Grid>
+
 
 );
 
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,21 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+
 
 import styles from './Tables.module.scss';
 
 const Tables = ({bookingId = 'BOK45er', eventId ='EV89zz'}) => (
 
-  <div className={styles.component}>
-    <h2>Tables view</h2>
-    <div>
-      <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} className={styles.link}>Booking New</Link>
-      <Link to={`${process.env.PUBLIC_URL}/tables/booking/${bookingId}`} className={styles.link}>Booking: {bookingId}</Link>
-      <Link to={`${process.env.PUBLIC_URL}/tables/events/new`} className={styles.link}>Event new</Link>
-      <Link to={`${process.env.PUBLIC_URL}/tables/events/${eventId}`} className={styles.link}>Event: {eventId}</Link>
-    </div>
-  </div>
+  <Grid container component='div' className={styles.component} justify='space-between' spacing={2}>
+    <CssBaseline />
+    <Grid item xs={12} className={styles.box}>
+      <Paper>
+        <Grid container component='div' className={styles.component} >
+          <Grid item xs={12} sm={7} className={styles.box}>
+            <form className={styles.datepicker} noValidate>
+              <TextField
+                id='datetime-local'
+                label='Choose date'
+                type='datetime-local'
+                defaultValue= '2021-01-29T14:30'
+                className={styles.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </form>
+          </Grid>
+          <Grid item xs={12} sm={5} className={styles.box}>
+            nowe pozycje
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
 
+
+
+    <Grid item xs={12} className={styles.box}>
+      <Paper className={styles.paper}>
+        tabela
+      </Paper>
+    </Grid>
+
+
+
+
+    <div className={styles.component}>
+      <h2>Tables view</h2>
+      <div>
+        <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} className={styles.link}>Booking New</Link>
+        <Link to={`${process.env.PUBLIC_URL}/tables/booking/${bookingId}`} className={styles.link}>Booking: {bookingId}</Link>
+        <Link to={`${process.env.PUBLIC_URL}/tables/events/new`} className={styles.link}>Event new</Link>
+        <Link to={`${process.env.PUBLIC_URL}/tables/events/${eventId}`} className={styles.link}>Event: {eventId}</Link>
+      </div>
+    </div>
+
+  </Grid>
 );
 
 Tables.propTypes = {
