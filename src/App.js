a0b3a6c7bac25518031c73e81f0509a2aa74195e
rafdul <rafalduluk@gmail.com>
2021-02-05 14:20:18 +0100

    Implement Redux and API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/Homepage';
@@ -27,27 +29,29 @@ const theme = createMuiTheme({
 
 function App() {
   return (
-    <BrowserRouter basename={'/panel'}>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={BookingNew}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={BookingDetails}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={EventsNew}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={EventsDetails}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component={OrderNew}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={OrderDetails}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-      </StylesProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename={'/panel'}>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={BookingNew}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={BookingDetails}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={EventsNew}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={EventsDetails}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component={OrderNew}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={OrderDetails}/>
+                <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,40 @@
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import tablesReducer from './tablesRedux';
+
+// define initial state and shallow-merge initial data
+const initialState = {
+  tables: {
+    data: {}, //tablica stolików pobrana z API
+    loading: {
+      active: false,
+      error: false,
+    }, //loading zawiera informacje o wczytywaniu danych – active mówi nam, czy trwa wczytywanie, a error będzie zawierał ew. komunikat o błędzie
+  },
+};
+
+// define reducers
+const reducers = {
+  tables: tablesReducer,
+};
+
+// add blank reducers for initial state properties without reducers
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+const combinedReducers = combineReducers(reducers);
+
+// create store
+const store = createStore(
+  combinedReducers,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+export default store;
